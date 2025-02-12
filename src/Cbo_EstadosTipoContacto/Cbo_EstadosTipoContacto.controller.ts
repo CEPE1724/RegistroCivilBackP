e23a724715b676272207077f187f5d2da64541a4
CEPE1724 <edisonnacional1@gmail.com>
2025-02-12 10:05:06 -0500

    PRUEB DE COMBO 3

--- src/Cbo_EstadosTipoContacto/Cbo_EstadosTipoContacto.controller.ts
+++ src/Cbo_EstadosTipoContacto/Cbo_EstadosTipoContacto.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { Cbo_EstadosTipoContactoService } from "./Cbo_EstadosTipoContacto.service";
 import { Cbo_EstadosTipoContactoEntity } from "./Cbo_EstadosTipoContacto.entity";
 import { ResponseDto } from "./Cbo_EstadosTipoContacto.dto";
@@ -9,8 +9,8 @@ export class Cbo_EstadosTipoContactoController {
     constructor(private readonly service: Cbo_EstadosTipoContactoService) {}
 
     @Get()
-    async findOne(@Param('idCbo_EstadoGestion') idCbo_EstadoGestion: number): Promise<ResponseDto<Cbo_EstadosTipoContactoEntity>> {
-        const data = await this.service.findOne(idCbo_EstadoGestion);
+    async find(@Query('idCbo_EstadoGestion') idCbo_EstadoGestion: number): Promise<ResponseDto<any>> {
+        const data = await this.service.find(idCbo_EstadoGestion);
         if (!data) {
             return { statusCode: 400, message: 'No data found', data: null };
         }
