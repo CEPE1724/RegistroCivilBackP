342e6f5e1ab2463aa008dcb53cf17a839cfbc42f
Isabela Tejada <isabelatejada@IsisMacBookPro.mshome.net>
2025-04-02 12:37:30 -0500

    api para info trabajo

---
+++ src/terrena-gestion-trabajo/entities/terrena-gestion-trabajo.entity.ts
@@ -0,0 +1,62 @@
+// terrena-gestion-trabajo.entity.ts
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('TerrenaGestionTrabajo')
+export class TerrenaGestionTrabajo {
+  @PrimaryGeneratedColumn()
+  idTerrenaGestionTrabajo: number;
+
+  @Column()
+  idClienteVerificacion: number;
+
+  @Column()
+  idTerrenaTipoTrabajo: number;
+
+  @Column()
+  iTiempoTrabajo: number;
+
+  @Column()
+  iTiempoTrabajoYear: number;
+
+  @Column('decimal', { precision: 18, scale: 2 })
+  dIngresoTrabajo: number;
+
+  @Column({ type: 'varchar', length: 500, nullable: true })
+  DireccionesVisitada: string;
+
+  @Column('float')
+  Latitud: number;
+
+  @Column('float')
+  Longitud: number;
+
+  @Column({ type: 'varchar', length: 1000, nullable: true })
+  trabajoImages: string;
+
+  @Column('datetime')
+  FechaSistema: Date;
+
+  @Column({ type: 'varchar', length: 50 })
+  Estacion: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  Usuario: string;
+
+  @Column({ type: 'varchar', length: 300 })
+  CallePrincipal: string;
+
+  @Column({ type: 'varchar', length: 300 })
+  CalleSecundaria: string;
+
+  @Column({ type: 'varchar', length: 500, nullable: true })
+  ActividadTrabajo: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  TelefonoTrabajo: string;
+
+  @Column({ type: 'varchar', length: 500, nullable: true })
+  PuntoReferencia: string;
+
+  @Column({ type: 'varchar', length: 500, nullable: true })
+  PersonaEntrevistada: string;
+}
---
+++ src/terrena-gestion-trabajo/terrena-gestion-trabajo.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTerrenaGestionTrabajoDto } from './dto/create-terrena-gestion-trabajo.dto';
+import { UpdateTerrenaGestionTrabajoDto } from './dto/update-terrena-gestion-trabajo.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TerrenaGestionTrabajo } from './entities/terrena-gestion-trabajo.entity';
+import { Logger } from '@nestjs/common';
+@Injectable()
+export class TerrenaGestionTrabajoService {
+private readonly logger = new Logger('TerrenaGestionTrabajoService');
+ constructor(
+  @InjectRepository(TerrenaGestionTrabajo)
+  private readonly terrenaRepo: Repository<TerrenaGestionTrabajo>,
+  ) {}
+
+ async findOne(id: number) {
+  return await this.terrenaRepo.findOne({ where: { idTerrenaGestionTrabajo: id } });
+ }
+
+}
+
+
